e24209cbecd721d39a8acdb30f2827758543c578
Sebastian <sebastianurwan@gmail.com>
2019-12-08 20:16:49 +0100

    Update GameOver.java
    Internal frame showing current player's score.

--- src/classes/GameOver.java
+++ src/classes/GameOver.java
@@ -1,8 +1,27 @@
 package classes;
 
+import java.awt.event.*;
+import java.io.IOException;
+import javax.swing.*;
+
 public class GameOver extends AbstractDialog{
     
-    public GameOver(){
-        super();
+    public GameOver() throws IOException{
+        super(200, 200); 
+        new ScoreSave().save(MainFrame.nickname, Integer.toString(MainFrame.points));
+                
+        label.setText("Your score " + Integer.toString(MainFrame.points));
+        button.setText("Play again");
+
+        panel.add(Box.createVerticalStrut(65));
+        panel.add(label);                
+        panel.add(Box.createVerticalStrut(10));
+        panel.add(button);        
+        setUpPanel();
     }    
+    
+    public void buttonMouseClicked(MouseEvent evt) {                                                      
+        dispose();
+        MainFrame.points = 0;
+    }                                     
 }
