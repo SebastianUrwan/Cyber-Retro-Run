b97d65ffbea0904c5b95b0cf3ca045dd112b6bad
Sebastian <sebastianurwan@gmail.com>
2019-12-08 20:32:56 +0100

    Create Heart.java
    Display and management player's lifes.

---
+++ src/classes/Heart.java
@@ -0,0 +1,25 @@
+package classes;
+
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+import javax.imageio.ImageIO;
+import javax.swing.*;
+
+public class Heart extends JLabel{
+
+    BufferedImage img = ImageIO.read(MainFrame.class.getResourceAsStream("/retrorun/hearts2.png"));
+    final public int labelX = 283;
+    final public int labelY = 430;
+    final public int labelWidth = 74;
+    final public int labelHeight = 50;
+    
+    public Heart() throws IOException{
+        super();
+        this.setOpaque(false);
+        setImg(3);
+    }
+    
+    public void setImg(int life){
+        this.setIcon(new ImageIcon(img.getSubimage(0, (life*50) - 50, labelWidth, labelHeight)));
+    }
+}
